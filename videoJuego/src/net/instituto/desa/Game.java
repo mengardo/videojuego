bf9fd7685ef440b13b85021b6e1c1eb510915120
Raul Rodriguez <tortosaraul@gmail.com>
2022-05-20 18:47:24 +0200

    Pregunta 2

--- videoJuego/src/net/instituto/desa/Game.java
+++ videoJuego/src/net/instituto/desa/Game.java
@@ -1,20 +1,25 @@
 package net.instituto.desa;
 
 public class Game {
+	private static final int AVANCE = 1;
+	private static final String ABAJO = "Abajo";
+	private static final String ARRIBA = "Arriba";
+	private static final String IZQUIERDA = "Izquierda";
+	private static final String DERECHA = "Derecha";
 	Jugador j;
 
 	public void movimiento(String m) {
-		if (m.equalsIgnoreCase("Derecha")) {
-			j.setX(j.getX() + 1);
+		if (m.equalsIgnoreCase(DERECHA)) {
+			j.setX(j.getX() + AVANCE);
 		}
-		if (m.equalsIgnoreCase("Izquierda")) {
-			j.setX(j.getX() - 1);
+		if (m.equalsIgnoreCase(IZQUIERDA)) {
+			j.setX(j.getX() - AVANCE);
 		}
-		if (m.equalsIgnoreCase("Arriba")) {
-			j.setY(j.getY() - 1);
+		if (m.equalsIgnoreCase(ARRIBA)) {
+			j.setY(j.getY() - AVANCE);
 		}
-		if (m.equalsIgnoreCase("Abajo")) {
-			j.setY(j.getY() + 1);
+		if (m.equalsIgnoreCase(ABAJO)) {
+			j.setY(j.getY() + AVANCE);
 		}
 	}
 }
