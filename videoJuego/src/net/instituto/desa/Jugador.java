0bd738690d49436ceb8e4233b28144c4058c2290
Raul Rodriguez <tortosaraul@gmail.com>
2022-05-20 18:44:25 +0200

    Pregunta 1

--- videoJuego/src/net/instituto/desa/Jugador.java
+++ videoJuego/src/net/instituto/desa/Jugador.java
@@ -3,19 +3,19 @@ package net.instituto.desa;
 public class Jugador {
 	int y, x;
 
-	public int getY() {
+	private int getY() {
 		return y;
 	}
 
-	public void setY(int y) {
+	private void setY(int y) {
 		this.y = y;
 	}
 
-	public int getX() {
+	private int getX() {
 		return x;
 	}
 
-	public void setX(int x) {
+	private void setX(int x) {
 		this.x = x;
 	}
 
